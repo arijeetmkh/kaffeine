47e089a51636d68dd6479c71c219db8f5cfbb6d9
Arijeet Mukherjee <arijeet_in@yahoo.co.in>
2014-02-15 17:19:09 +0530

    Bug fix on QueryFactory Assembler

--- kaffeine/kaffeine/settings.py
+++ kaffeine/kaffeine/settings.py
@@ -134,7 +134,7 @@ INSTALLED_APPS = (
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
+SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
